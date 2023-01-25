57b3bfae7794ce30d6fff37a633ec284e4f115a4
Nelson Venegas <nelson.venegasg@gmail.com>
2023-01-25 16:33:56 -0300

    change var for text in script

--- db/script_db.sql
+++ db/script_db.sql
@@ -8,22 +8,21 @@ CREATE DATABASE notas_app;
 -- create table user
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL UNIQUE,
-  username VARCHAR(20) NOT NULL UNIQUE,
-  password VARCHAR(50) NOT NULL CHECK (length(password) >= 8)
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL CHECK (length(password) >= 8)
 );
 
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY,
-  title_task VARCHAR(50) NOT NULL,
-  description VARCHAR(200) NOT NULL,
-  image_task VARCHAR(255),
+  title_task TEXT NOT NULL,
+  description TEXT NOT NULL,
+  image_task TEXT,
   date TIMESTAMP NOT NULL,
   status_task BOOLEAN NOT NULL,
   user_id INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-INSERT INTO users (first_name, last_name, email, username, password) VALUES ("piero", "venegas", "piero@gmail.com", "pipo", "12345678");
