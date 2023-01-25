b8050d2b2f4078bb974c27371db40b6c157719f7
Nelson Venegas <nelson.venegasg@gmail.com>
2023-01-24 22:22:48 -0300

    fixed updated note error

--- db/queries.db.js
+++ db/queries.db.js
@@ -6,6 +6,7 @@ const pool = new Pool(db);
 
 module.exports = {
   newUser: async (firstName, lastName, email, username, password) => {
+    console.log({ firstName, lastName, email, username, password });
     try {
       const consult = {
         text: `INSERT INTO users(first_name, last_name, email, username, password) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
